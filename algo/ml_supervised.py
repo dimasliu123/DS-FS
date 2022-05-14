374a4d1e6c9156b3f6f9830843e833032d4307d5
dimasliu123 <dimasyuliusa@gmail.com>
2022-05-14 20:04:45 +0700

    added mini batch gd on Logistic and Softmax Regression

--- algo/ml_supervised.py
+++ algo/ml_supervised.py
@@ -96,24 +96,23 @@ class KNearestNeighbor:
 # Logistic Regression
 class LogisticRegression :
     def __init__(self, 
-                 steps : int = 100, 
+                 steps : int = 300, 
                  epsilon : float = 1e-6,
                  lr : float = 0.01,
                  threshold : float = 0.5,
                  use_bias : bool = True,
-                 init : str = "normal", 
-                 show_steps : bool = False):
+                 init : str = "normal"):
         self.use_bias = use_bias
         self.steps = steps
         self.epsilon = epsilon
         self.init = init.lower()
         self.lr = lr
-        self.show_steps = show_steps
         assert threshold < 1.0 and threshold > 0.0, f"Threshold has to be between 0 and 1 !"
         self.threshold = threshold
     
-    def fit(self, X, y):
+    def fit(self, X, y, batch_size=64):
         X, y = np.array(X), np.array(y)
+        assert batch_size <= len(X), "Batch size can't be bigger than size of the data"
         assert len(np.unique(y)) == 2, "Logistic Regression can only be used as binary classification. Use Softmax Regression instead."
         N, m = X.shape
         if self.init == "normal":
@@ -124,44 +123,51 @@ class LogisticRegression :
             b = np.random.uniform(low=-0.05, high=0.05, size=1)
         else :
             raise ValueError("Weights initializer is not valid. Use uniform or normal.")
-        assert X.shape[0] == y.shape[0], f"Feature size {X.shape[0]} has not the same as label size {y.shape[0]}"            
+        assert len(X) == len(y), f"Feature size {len(X)} has not the same as label size {len(y)}"            
         losses = []
         
         for _ in range(self.steps):
-            if self.use_bias :
-                y_prob = LogisticRegression.sigmoid(np.dot(X, w) + b)
-                dw = (1 / m) * np.dot(X.T, (y_prob - y)) # Calculate Gradient Descent for the weights
-                db = (1 / m) * np.sum(y_prob - y) # Calculate Gradient Descent for the bias
-                w = w - self.lr * dw
-                b = b - self.lr * db
-                loss = LogisticRegression.logloss(y, y_prob, self.epsilon)
-                if self.show_steps:
-                    print(f"Steps : {_ + 1} => Loss : {loss}")
-                losses.append(loss)
-            else : 
-                y_prob = LogisticRegression.sigmoid(np.dot(X, w)) # feedforward
-                dw = (1 / m) * np.dot(X.T, (y_prob - y))
-                w = w - self.lr * dw
-                loss = LogisticRegression.logloss(y, y_prob, self.epsilon)
-                if self.show_steps :
-                    print(f"Steps : {_ + 1} => Loss : {loss}")
-                losses.append(loss)
+            perm = np.random.permutation(N)
+            X_shuf, y_shuf = X[perm], y[perm]           
+            for batch in range(0, N, batch_size):
+                X_batch, y_batch = X_shuf[batch:batch+batch_size], y_shuf[batch:batch+batch_size]
+                if self.use_bias :
+                    y_prob = self.__sigmoid(np.dot(X_batch, w) + b)
+                    dw, db = self.__gradientDescent(X_batch, y_batch, y_prob)
+                    w = w - self.lr * dw
+                    b = b - self.lr * db
+                    loss = self.__logloss(y_batch, y_prob, self.epsilon)
+                    losses.append(loss)
+                else : 
+                    y_prob = self.__sigmoid(np.dot(X_batch, w)) # feedforward
+                    dw = self.__gradientDescent(X_batch, y_batch, y_prob)
+                    w = w - self.lr * dw
+                    loss = self.__logloss(y_batch, y_prob, self.epsilon)
+                    losses.append(loss)
         self.w, self.b = w, b
+        self.loss_hist = np.array(losses)
         
     def predict(self, X):
         assert X.shape[1] == len(self.w), "Different shape with fitted data !"
         if self.use_bias :
-            z = LogisticRegression.sigmoid(np.dot(X, self.w) + self.b )
+            z = self.__sigmoid(np.dot(X, self.w) + self.b )
         else : 
-            z = LogisticRegression.sigmoid(np.dot(X, self.w))
+            z = self.__sigmoid(np.dot(X, self.w))
         return np.array([1 if i > self.threshold else 0 for i in z])
+    
+    def __gradientDescent(self, X, y_true, y_hat):
+        if self.use_bias : 
+            dw = (1 / len(X) * np.dot(X.T, (y_hat - y_true))) # Calculate Gradient Descent for the weights
+            db = (1 / len(X) * np.sum(y_hat - y_true)) # Calculate Gradient Descent for the bias
+            return dw, db
+        else :
+            dw = (1 / len(X) * np.dot(X.T, (y_hat - y_true)))
+            return dw
         
-    @staticmethod
-    def sigmoid(z):
+    def __sigmoid(self, z):
         return 1 / ( 1 + np.exp(-z))
         
-    @staticmethod
-    def logloss(y_true, y_pred, epsilon):
+    def __logloss(self, y_true, y_pred, epsilon):
         y_pred = np.clip(y_pred, a_min = epsilon, a_max = 1 - epsilon)
         notation1 = y_true * np.log(y_pred + epsilon)
         notation2 = ( 1 - y_true) * np.log(1 - y_pred + epsilon)
@@ -171,22 +177,20 @@ class LogisticRegression :
 # Softmax Regression
 class SoftmaxRegression :
     def __init__(self, 
-                 steps : int = 100, 
+                 steps : int = 300, 
                  lr : float = 0.01,
                  use_bias : bool = True,
-                 epsilon : float = 1e-7,
-                 init : str = "normal", 
-                 show_steps : bool = False):
+                 init : str = "normal"):
         self.steps = steps
         self.lr = lr
         self.use_bias = use_bias
         self.init = init.lower()
-        self.show_steps = show_steps
         
-    def fit(self, X, y):
+    def fit(self, X, y, batch_size = 32):
         X, y = np.array(X), np.array(y)
+        assert batch_size <= len(X), "Batch size can't be bigger than size of the data."
         assert len(X) == len(y), f"Feature size {len(X)} has different size with label size len(y)"
-        y_ohe = SoftmaxRegression.OneHot(y)
+        y_ohe = self.__OneHot(y)
         losses = []
         N, m = X.shape
         if self.init == "normal":
@@ -199,51 +203,59 @@ class SoftmaxRegression :
             raise ValueError("Weights initializer is not valid.. Use normal or uniform.")
         
         for _ in range(self.steps):
-            if self.use_bias :
-                z = SoftmaxRegression.softmax(np.dot(X, w)) + b
-                dw = (1 / m) * np.dot(X.T, (z - y_ohe))
-                db = (1 / m) * np.sum(z - y_ohe)
-                w -= self.lr * dw
-                b -= self.lr * db
-                loss = SoftmaxRegression.categoryLogLoss(y, z)
-                if self.show_steps :
-                    print(f"Epochs : {_ + 1} => Loss : {loss}")
-            else :
-                z = SoftmaxRegression.softmax(np.dot(X, w))
-                dw = (1 / m) * np.dot(X.T, (z - y_ohe))
-                w -= self.lr * dw
-                loss = SoftmaxRegression.categoryLogLoss(y, z)
-                if self.show_steps :
-                    print(f"Epochs: {_ + 1} => Loss : {loss}")
-            losses.append(loss)
-        self.m = m
+            perm = np.random.permutation(N)
+            X_perm, y_perm, y_perm_ohe = X[perm], y[perm], y_ohe[perm]
+            for batch in range(0, N, batch_size):
+                X_batch, y_batch, y_ohe_batch = X_perm[batch:batch+batch_size], y_perm[batch:batch+batch_size], y_perm_ohe[batch:batch+batch_size]
+                if self.use_bias :
+                    y_prob = self.__softmax(np.dot(X_batch, w)) + b
+                    dw, db = self.__gradientDescent(X_batch, y_ohe_batch, y_prob)
+                    w = w - self.lr * dw
+                    b = b - self.lr * db
+                    loss = self.__categoryLogLoss(y_batch, y_prob)
+                    losses.append(loss)
+                     
+                else :
+                    z = self.__softmax(np.dot(X_batch, w))
+                    dw = self.__gradientDescent(X_batch, y_ohe_batch, y_prob)
+                    w -= self.lr * dw
+                    loss = SoftmaxRegression.categoryLogLoss(y_batch, y_prob)
+                    losses.append(loss)
+
         self.w, self.b = w, b
+        self.loss_hist = np.array(losses)
         
     def predict(self, X):
         X = np.array(X)
         assert X.shape[1] == self.m, f"{X.shape[1]} has not the same shape as fit !"
         if self.use_bias : 
-            z = SoftmaxRegression.softmax(np.dot(X, self.w))
+            z = self.__softmax(np.dot(X, self.w))
         else :
-            z = SoftmaxRegression.softmax(np.dot(X, self.w))
+            z = self.__softmax(np.dot(X, self.w))
         return np.argmax(z, axis=-1)
+    
+    def __gradientDescent(self, X, y_true, y_hat):
+        if self.use_bias : 
+            dw = (1 / len(X)) * np.dot(X.T, (y_hat - y_true))
+            db =  (1 / len(X)) * np.sum(y_hat - y_true)
+            return dw, db
+        else : 
+            dw = (1 / len(X)) * np.dot(X.T, (y_hat - y_true))
+            return dw
         
-    @staticmethod
-    def softmax(z):
+    def __softmax(self, z):
         exp_z = np.exp(z)
         return exp_z / exp_z.sum()
     
-    @staticmethod
-    def OneHot(y):
+    def __OneHot(self, y):
         y_ohe = np.zeros((len(y), len(np.unique(y))))
         y_ohe[np.arange(len(y)), y] = 1
         return y_ohe
     
-    @staticmethod
-    def categoryLogLoss(y_true, y_pred):
+    def __categoryLogLoss(self, y_true, y_pred):
         return - np.mean(np.log(y_pred[np.arange(len(y_true)), y_true]))
 
-
+# Learning Vector Quantization
 class LearningVectorQuantization: # x = x + lr * (t - x ), lr = a * ( 1 - (epoch/max_epoch)) 
     def __init__(self, lr :float = 0.01, epochs : int = 100): # measuring similarity : || xi - wi || comp neural network
         self.lr = lr
@@ -264,3 +276,4 @@ class LearningVectorQuantization: # x = x + lr * (t - x ), lr = a * ( 1 - (epoch
         for i in range(X1):
             dist += ((X1[i] - X2[i]) ** 2)
         return np.sqrt(dist)
+
